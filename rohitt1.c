1f679820d2b5904d549390db9590d36317e2c1ff
rohittnishant <36066907+rohittnishant@users.noreply.github.com>
2018-02-02 13:58:54 +0530

    Create rohitt1.c

---
+++ rohitt1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+int a;
+clrscr();
+printf("Enter the number");
+scanf("%d",&a);
+if (a%2==0)
+{
+printf("the number is even number ");
+else
+printf("the number is odd");
+}
+return 0;
+}
