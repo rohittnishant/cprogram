655fd02cc546a1af3e54b02d8ba0c8c75ba204ed
rohittnishant <36066907+rohittnishant@users.noreply.github.com>
2018-02-09 13:49:28 +0530

    Create nishant392.c

---
+++ nishant392.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int r[5],b={2,3,5,7,11};
+if(r==b)
+printf(" \n The Number is Prime Number");
+scanf("%d",&r);
+printf(" \n The Number Is Not Prime Number");
+scanf("%d",b);
+return 0;
+}
