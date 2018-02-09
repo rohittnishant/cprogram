f0dd25d5d4ddfd9d989378122465ad05a3a5fb04
rohittnishant <36066907+rohittnishant@users.noreply.github.com>
2018-02-09 14:07:55 +0530

    Create nishant792.c

---
+++ nishant792.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>]
+int main()
+{
+ int 371,p,r,res= 0;
+printf("Enter a three digit num\n");
+scanf("%d", &p);
+371=num;
+while(371!=0)
+r=371;
+r*r*r;
+371/=10;
+if(res=p)
+printf("%d is an Armstrong number",p);
+else
+printf("%d is not an Armstrong number",p);
+getch();
+return 0;
+}
