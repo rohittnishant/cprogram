121aaefaeb4fecf7c25558c093166f8556f1ff4b
rohittnishant <36066907+rohittnishant@users.noreply.github.com>
2018-02-09 13:30:32 +0530

    Create rohitt1192.c

---
+++ rohitt1192.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int n, h, sum = 0;
+    printf("Enter a value\n ");
+    scanf("%d",&n);
+    for(h=1; h <= n; ++h)
+    {
+        sum += i;   
+    }
+    printf("Sum = %d",sum);
+    return 0;
+}
