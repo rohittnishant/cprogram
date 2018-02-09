48afb3ed2740d91cc918284d9c42bbd8dc57ce81
rohittnishant <36066907+rohittnishant@users.noreply.github.com>
2018-02-09 13:43:14 +0530

    Create nishant192.c

---
+++ nishant192.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,s,h;
+printf("Enter the value \n");
+scanf("%d",&a);
+printf(" Enter the value \n");
+scanf("%d",&s);
+c=pow(a,s);
+printf("%d \n",h);
+getch();
+return 0;
+}
