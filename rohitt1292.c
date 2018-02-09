f93a88a1a160da30805cc318f0a5b6dbcb85d38d
rohittnishant <36066907+rohittnishant@users.noreply.github.com>
2018-02-09 13:33:35 +0530

    Create rohitt1292.c

---
+++ rohitt1292.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main ()
+{
+	int n,f,k,sum=0,v[100];
+	scanf("%d\n",&n);
+	scanf("%d\n",&k);
+	for(f=0;f<n;f++)
+	{
+            scanf("%d",&v[i]);
+	}
+	for(f=0;f<k;f++)
+	{
+		sum+=v[i];
+	}
+	
+printf("%d",sum);
+	return 0;
+	
+}
